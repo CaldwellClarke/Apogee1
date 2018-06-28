541a54556f24ff02a2204b0d7bf8c7fc1870d368
Malekagr <maa245@pitt.edu>
2018-06-28 13:01:38 -0400

    new reqs

--- apogee1/settings/local.py
+++ apogee1/settings/local.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 from decouple import config
 import dj_database_url
+import django-heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # points us back to the root folder, where manage.py is
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -191,6 +192,7 @@ MEDIA_URL = '/media/'
 # this just works with crispy form to render properly
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+django_heroku.settings(locals())
 
 
 
